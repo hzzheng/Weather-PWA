239d702d4a645518bf8e391db27841286a417355
hzzheng <zhchaozju@gmail.com>
2017-09-21 15:01:38 +0800

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+})
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log('listening')
+})
